aaf4cefc9b7f92b667bfeed182e36f8e75f05bca
andrey <polyakovv.andrey.01@gmail.com>
2023-12-10 09:51:13 +0200

    added endpoint for updating catalog items

--- src/catalog/crud.py
+++ src/catalog/crud.py
@@ -1,10 +1,47 @@
+import os
+
+from fastapi import HTTPException, status, UploadFile
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.catalog.models import CatalogItem
+from src.utils import upload_image
 
 
 async def add_catalog_item_to_db(session: AsyncSession, item_name: str, item_image_url: str) -> CatalogItem:
     catalog_item = CatalogItem(item_name=item_name, item_image_url=item_image_url)
     session.add(catalog_item)
     await session.commit()
+    return catalog_item
+
+
+async def get_catalog_item(item_id: int, session: AsyncSession) -> CatalogItem:
+    stmt = select(CatalogItem).where(CatalogItem.id == item_id)
+    catalog_item: None | CatalogItem = await session.scalar(stmt)
+    await session.close()
+    if catalog_item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Catalog item not found!")
+    return catalog_item
+
+
+async def update_catalog_item_in_db(
+        catalog_item: CatalogItem,
+        session: AsyncSession,
+        item_name: str | None,
+        image: UploadFile | None,
+) -> CatalogItem:
+    if item_name:
+        catalog_item.item_name = item_name
+    if image:
+        if catalog_item.item_image_url:
+            current_image_path = catalog_item.item_image_url.split("/")[-1]
+            current_image_path = os.path.join("media/images/catalog", current_image_path)
+            if os.path.exists(current_image_path):
+                os.remove(current_image_path)
+
+        image_url = await upload_image("catalog", image)
+        catalog_item.item_image_url = image_url
+    session.add(catalog_item)
+    await session.commit()
+
     return catalog_item
--- src/catalog/routers.py
+++ src/catalog/routers.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, Form, UploadFile, File
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Form, UploadFile, File, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.services import get_admin
-from src.catalog.crud import add_catalog_item_to_db
+from src.catalog.crud import add_catalog_item_to_db, get_catalog_item, update_catalog_item_in_db
 from src.catalog.schemas import ReadCatalogItem
 from src.utils import upload_image
 from src.database import db
@@ -22,3 +24,18 @@ async def create_catalog_item(
         session=session, item_name=item_name, item_image_url=image_url
     )
     return catalog_item
+
+
+@catalog_router.patch("/update-catalog-item/{item_id}/", response_model=ReadCatalogItem)
+async def update_catalog_item(
+        item_id: int = Path(...),
+        session: AsyncSession = Depends(db.scoped_session_dependency),
+        item_name: Optional[str] = Form(None),
+        image: UploadFile = File(default=None),
+        is_admin: dict = Depends(get_admin),
+):
+    catalog_item = await get_catalog_item(item_id, session)
+    updated_catalog_item = await update_catalog_item_in_db(
+        catalog_item=catalog_item, session=session, item_name=item_name, image=image
+    )
+    return updated_catalog_item
