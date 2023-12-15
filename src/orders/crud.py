8fe7a107ad6b5b860a6ad4eda1f8031fe889573a
andrey <polyakovv.andrey.01@gmail.com>
2023-12-15 09:40:44 +0200

    implemented all order endpoints

--- src/orders/crud.py
+++ src/orders/crud.py
@@ -1,48 +1,70 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import User
 from src.basket.models import Goods, Customer
 from src.logistic.models import DeliveryDetail, NovaPosta
+from src.orders.models import Order
 from src.product.models import Product
+
+
+async def get_order_from_bd(session: AsyncSession, order: Order):
+    delivery_detail = await session.get(DeliveryDetail, order.delivery_detail_id)
+    nova_posta = await session.get(NovaPosta, delivery_detail.nova_posta_id)
+    delivery_detail_data = {
+        "first_name": delivery_detail.first_name,
+        "last_name": delivery_detail.last_name,
+        "phone": delivery_detail.phone,
+        "nova_posta": nova_posta
+    }
+
+    goods_stmt = select(Goods).where(Goods.orders.any(id=order.id))
+    goods_result = await session.scalars(goods_stmt)
+    goods_data = []
+    for goods in goods_result:
+        product = await session.get(Product, goods.product_id)
+        goods_data.append({
+            "id": goods.id,
+            "amount": goods.amount,
+            "product": product
+
+        })
+
+    customer = await session.get(Customer, order.customer_id)
+    user = await session.get(User, customer.user_id) if customer.user_id else None
+    customer_email = user.email if user else None
+
+    order_data = {
+        "order_id": order.id,
+        "customer_email": customer_email,
+        "execution_status": order.execution_status,
+        "delivery_detail": delivery_detail_data,
+        "goods": goods_data
+    }
+    await session.close()
+    return order_data
 
 
 async def enrich_order_details(session: AsyncSession, orders):
     enriched_orders = []
     for order in orders:
-
-        delivery_detail = await session.get(DeliveryDetail, order.delivery_detail_id)
-        nova_posta = await session.get(NovaPosta, delivery_detail.nova_posta_id)
-        delivery_detail_data = {
-            "first_name": delivery_detail.first_name,
-            "last_name": delivery_detail.last_name,
-            "phone": delivery_detail.phone,
-            "nova_posta": nova_posta
-        }
-
-        goods_stmt = select(Goods).where(Goods.orders.any(id=order.id))
-        goods_result = await session.scalars(goods_stmt)
-        goods_data = []
-        for goods in goods_result:
-            product = await session.get(Product, goods.product_id)
-            goods_data.append({
-                "id": goods.id,
-                "amount": goods.amount,
-                "product": product
-
-            })
-
-        customer = await session.get(Customer, order.customer_id)
-        user = await session.get(User, customer.user_id) if customer.user_id else None
-        customer_email = user.email if user else None
-
-        order_data = {
-            "order_id": order.id,
-            "customer_email": customer_email,
-            "execution_status": order.execution_status,
-            "delivery_detail": delivery_detail_data,
-            "goods": goods_data
-        }
-
+        order_data = await get_order_from_bd(session, order)
         enriched_orders.append(order_data)
     return enriched_orders
+
+
+async def change_order_status(
+        order_status: str,
+        order_id: int,
+        session: AsyncSession
+):
+    order = await session.get(Order, order_id)
+    if not order:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    order.execution_status = order_status
+    session.add(order)
+    await session.commit()
+    order = await get_order_from_bd(session, order)
+    return order
--- src/orders/routers.py
+++ src/orders/routers.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Path, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import User
 from src.auth.services import get_current_user, get_admin
 from src.basket.crud import get_or_create_customer
 from src.basket.models import Customer
+from src.orders.models import Order
 from src.database import db
-from src.orders.crud import enrich_order_details
+from src.orders.crud import enrich_order_details, change_order_status, get_order_from_bd
 from src.orders.models import Order
 from src.utils import pagination_dependency, get_list_objects
 
@@ -27,17 +28,34 @@ async def get_my_order(
     return {"count": list_orders["count"], "result": enriched_orders}
 
 
-#
-# async def get_all_order(
-#         pagination: dict = Depends(pagination_dependency),
-#         admin: User = Depends(get_admin),
-#         session: AsyncSession = Depends(db.scoped_session_dependency())
-# ):
-#     pass
-#
-#
-# async def update_order_status(
-#         admin: User = Depends(get_admin),
-#         session: AsyncSession = Depends(db.scoped_session_dependency())
-# ):
-#     pass
+@order_router.get("/")
+async def get_all_order(
+        pagination: dict = Depends(pagination_dependency),
+        session: AsyncSession = Depends(db.scoped_session_dependency),
+        admin: User = Depends(get_admin),
+):
+    list_orders = await get_list_objects(session, pagination, Order)
+    enriched_orders = await enrich_order_details(session, list_orders["result"])
+    return {"count": list_orders["count"], "result": enriched_orders}
+
+
+@order_router.get("/{order_id}/")
+async def get_order(
+        order_id: int = Path(...),
+        admin: User = Depends(get_admin),
+        session: AsyncSession = Depends(db.scoped_session_dependency)
+):
+    order = await session.get(Order, order_id)
+    response = await get_order_from_bd(session, order)
+    return response
+
+
+@order_router.patch("/change_order_status/{order_id}/")
+async def update_order_status(
+        order_status: Order.ExecutionStatus = Query(..., description="New status of the order"),
+        order_id: int = Path(...),
+        admin: User = Depends(get_admin),
+        session: AsyncSession = Depends(db.scoped_session_dependency)
+):
+    response = await change_order_status(order_status, order_id, session)
+    return response
